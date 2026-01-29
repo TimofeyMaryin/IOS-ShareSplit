29a59b9ad2a1b535f486e815c53709d94f2b2559
Timofey Maryin <nemotim4ic@gmail.com>
2026-01-29 12:58:47 +0100

    Initial Commit

---
+++ Project-28-01-26-2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project-28-01-26-2
+//
+//  Created by Timofey on 29. 1. 2026..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project-28-01-26-2/Project_28_01_26_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_28_01_26_2App.swift
+//  Project-28-01-26-2
+//
+//  Created by Timofey on 29. 1. 2026..
+//
+
+import SwiftUI
+
+@main
+struct Project_28_01_26_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
